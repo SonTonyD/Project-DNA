2256d28711dafb3fa3eb49d02d45eb559ba16bdd
SonTonyD <sontony.dinh@gmail.com>
2022-12-06 22:31:10 +0100

    Movement and Camera

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DNA
+{
+    public class CameraHandler : MonoBehaviour
+    {
+        public Transform targetTransform;
+        public Transform cameraTransform;
+        public Transform cameraPivotTransform;
+        private Transform myTransform;
+        private Vector3 cameraTransformPosition;
+        private LayerMask ignoreLayers;
+
+        public static CameraHandler singleton;
+
+        public float lookSpeed = 0.1f;
+        public float followSpeed = 0.1f;
+        public float pivotSpeed = 0.03f;
+
+        private float defaultPosition;
+        private float lookAngle;
+        private float pivotAngle;
+        public float minimumPivot = -35;
+        public float maximumPivot = 90;
+
+        private void Awake()
+        {
+            singleton = this;
+            myTransform = transform;
+            defaultPosition = cameraTransform.localPosition.z;
+            ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        public void FollowTarget(float delta)
+        {
+            Vector3 targetPosition = Vector3.Lerp(myTransform.position, targetTransform.position, delta / followSpeed);
+            myTransform.position = targetPosition;
+        }
+
+        public void handleCameraRotation(float delta, float mouseXInput, float mouseYInput)
+        {
+            lookAngle += (mouseXInput * lookSpeed) / delta;
+            pivotAngle -= (mouseYInput * pivotSpeed) / delta;
+            pivotAngle = Mathf.Clamp(pivotAngle, minimumPivot, maximumPivot);
+
+            Vector3 rotation = Vector3.zero;
+            rotation.y = lookAngle;
+            Quaternion targetRotation = Quaternion.Euler(rotation);
+            myTransform.rotation = targetRotation;
+
+            
+
+            rotation = Vector3.zero;
+            rotation.x = pivotAngle;
+
+            targetRotation = Quaternion.Euler(rotation);
+            cameraPivotTransform.localRotation = targetRotation;
+        }
+    }
+}
+
+
